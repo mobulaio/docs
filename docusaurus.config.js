42810c728239d0a56ab3b5ca3005e9b02da675a6
Jason <jason@mobula.io>
2023-06-24 10:40:04 +0900

    revert conf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,6 +21,8 @@ const config = {
   organizationName: 'mobulaio', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
 
+  trailingSlash: false,
+
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
