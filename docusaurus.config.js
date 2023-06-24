88adbdf34a7d4ec0e03bf9e31452eacc47c5865a
Jason <jason@mobula.io>
2023-06-25 05:50:15 +0900

    trailing slash st to false

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'mobulaio', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  trailingSlash: true,
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
